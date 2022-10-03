db3a15205f4ef53b7719cd8f75809669639de1a5
Priyaraj7 <suppichala@gmail.com>
2022-10-02 22:30:32 -0700

    add testing

--- README.md
+++ README.md
@@ -45,3 +45,13 @@ Note: Server runs on http://localhost:4001 and client on http://localhost:3000
 ## Room for Improvement
 
 - write unit test using jest
+
+https://sammeechward.com/mocking-a-database-with-jest-in-javascript/
+
+https://medium.com/geoblinktech/postgres-and-integration-testing-in-node-js-apps-2e1b52af7ffc
+
+https://css-tricks.com/react-integration-testing-greater-coverage-fewer-tests/
+
+// https://handsonreact.com/docs/labs/js/T6-TestingForms
+
+// https://community.redwoodjs.com/t/testing-forms-using-testing-library-user-event/2058/8
--- server/app.spec.mjs
+++ server/app.spec.mjs
@@ -7,6 +7,7 @@ import router from "./routes";
 import { application } from "express";
 
 describe("Test the root path of index.mjs", () => {
+  // called before each of these tests (before every test function).
   beforeEach(async () => {
     await db.none("CREATE TEMPORARY TABLE users (LIKE users INCLUDING ALL)"); // This will copy constraints also
     await await db.one(
@@ -14,11 +15,11 @@ describe("Test the root path of index.mjs", () => {
       ["Ramesh", "ramesh@ramesh.com"]
     );
   });
-
+  // called after each of these tests (after every test function).
   afterEach(async function () {
     await db.none("DROP TABLE IF EXISTS pg_temp.users");
   });
-
+  // called once after all tests.
   afterAll(async () => {
     return db.$pool.end();
   });
@@ -36,13 +37,49 @@ describe("Test the root path of index.mjs", () => {
       .get("/users")
       .set("Accept", "application/json");
 
-    console.log(response.headers);
+    // console.log(response.headers);
     expect(response.headers["content-type"]).toEqual(
       expect.stringMatching("application/json")
     );
     expect(response.status).toEqual(200);
     expect(response.body[0].email).toEqual("ramesh@ramesh.com");
     expect(response.body[0].name).toEqual("Ramesh");
+  });
+
+  it("should able add new user", async () => {
+    const req = {
+      name: "josh",
+      email: "josh@gmail.com",
+    };
+    // await postUser(req);
+
+    // const { rows } = await db.one(
+    //   "INSERT INTO users(name, email) VALUES($1, $2) RETURNING *",
+    //   ["josh", "josh@gmail.com"]
+    // );
+    // expect(rows).lengthOf(1);
+    // const response = await request(app)
+    //   .post("/users")
+    //   .set({ name: "josh", email: "josh@gmail.com" });
+    // console.log(response);
+    // expect(response.status).toEqual(200);
+
+    // async function postUser(req, status = 200) {
+    //   const { body } = await request(app).post("/").send(req).expect(status);
+    //   return body;
+    // }
+
+    const newStudent = await request(app).post("/").send(req);
+    console.log(newStudent);
+
+    // make sure we add it correctly
+    //expect(newStudent.body).toHaveProperty("id");
+    expect(newStudent.body.name).toEqual("josh");
+    expect(newStudent.statusCode).toEqual(200);
+
+    // make sure we have 3 students now
+    const response = await request(app).get("/");
+    expect(response.body.length).toBe(3);
   });
 });
 
